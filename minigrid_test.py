3e026ec10418aab130e0a7d4aede71e970d0ead7
Wu Junliang <wujunlianges@outlook.com>
2019-12-31 15:13:06 +0000

    remove useless files

--- minigrid_test.py
+++ minigrid_test.py
@@ -6,9 +6,10 @@ from dopamine.discrete_domains import run_experiment
 
 import tensorflow as tf
 
-# gin_files = ['dopamine/agents/dqn/configs/dqn.gin']
-gin_files = ['./minigrid.gin']
-base_dir = './tmp/minigrid/'
+gin_files = ['dopamine/agents/dqn/configs/dqn.gin']
+base_dir = './tmp/dqn/'
+# gin_files = ['./minigrid.gin']
+# base_dir = './tmp/minigrid/'
 gin_bindings = []
 
 tf.logging.set_verbosity(tf.logging.INFO)
